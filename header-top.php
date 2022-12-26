8a488d1bbac3462d4d82f2ce513b3fefe712e282
OttavioM <OttavioM@users.noreply.github.com>
2022-12-26 18:15:50 +0100

    update
    Header mobile still moving up a little but the headeer llogo in mobile go to 0 when scroll down and the header in mobile is hidden. Then it is only an advertising bar, the menu should be small and diffreent, maybe with javascript

--- header-top.php
+++ header-top.php
@@ -26,10 +26,10 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pasol' ); ?></a>
 
-    <div class="announcement-bar pt-0.3 pb-0.3">
+    <div class="announcement-bar pt-0.3 pb-3">
         <div class="container">
             <div class="row">
-
+                <!-- FOR NOT MOBILE there is a menu -->
                 <div class="col-md-4 d-flex justify-content-start col-sm-12">
                     <ul class="announcement-bar__list" > 
                         <li>
@@ -52,9 +52,7 @@
                             <a href="<?php echo wc_get_cart_url(); ?>"><i class="bi bi-cart4"></i></a>
                             <a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
                         </div> 
-
                     </ul>
-
                 </div>
                 <!-- FOR MOBILE TEXT -->
                 <div class = "announcement-bar__mobile">
--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ $('#carouselExampleCaptions').carousel({
     interval: 2000
     
   })
+
+//
+// $(window).on("scroll", function () {
+//   if (document.body.scrollTop > 41 || document.documentElement.scrollTop > 41) {
+//     document.getElementsByClassName("site-header").style.top = "0px";
+//   } else {
+//     document.getElementById("masthead").style.top = "41px";
+//   }
+// });
