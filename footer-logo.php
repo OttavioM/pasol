2938ef82712264fb0e7350350619b0fa367b074d
OttavioM <OttavioM@users.noreply.github.com>
2023-02-01 23:52:03 +0100

    footer-logo chatgpt delete
    I delete the chat gpt  with links in array and the use the function

--- footer-logo.php
+++ footer-logo.php
@@ -13,25 +13,8 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<?php
-				$links = array(
-				  array(
-					array('text' => 'Link 1', 'href' => '/link-1'),
-					array('text' => 'Link 2', 'href' => '/link-2'),
-					array('text' => 'Link 3', 'href' => '/link-3'),
-					array('text' => 'Link 4', 'href' => '/link-4'),
-					array('text' => 'Link 5', 'href' => '/link-5')
-				  ),
-				  array(
-					array('text' => 'Link 6', 'href' => '/link-6'),
-					array('text' => 'Link 7', 'href' => '/link-7'),
-					array('text' => 'Link 8', 'href' => '/link-8'),
-					array('text' => 'Link 9', 'href' => '/link-9'),
-					array('text' => 'Link 10', 'href' => '/link-9'))
-				  );
-				
-			?>
-			<?php generateFooter($links) ?>
+
+
 		
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
