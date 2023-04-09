ac198aac7136eb5d48bf8e89cdddea967fca349c
OttavioM <OttavioM@users.noreply.github.com>
2023-04-09 20:03:14 +0200

    added animation in the 404.php
    Cavernicola page not found with the codepen helps

--- 404.php
+++ 404.php
@@ -14,51 +14,35 @@ get_header('logo');
 ?>
 <!-- ?> -->
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main pt-5">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'pasol' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'pasol' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'pasol' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$pasol_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'pasol' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$pasol_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+	<section class="page_404">
+		<div class="container">
+			<div class="row">	
+			<div class="col-sm-12 ">
+			<div class="col-sm-10 col-sm-offset-1  text-center">
+			<div class="four_zero_four_bg">
+				<h1 class="text-center ">404</h1>
+			
+			
+			</div>
+			
+			<div class="contant_box_404">
+			<h3 class="h2">
+			Look like you're lost
+			</h3>
+			
+			<p>the page you are looking for not avaible!</p>
+			
+			<a href="" class="link_404">Go to Home</a>
+		</div>
+			</div>
+			</div>
+			</div>
+		</div>
+	</section>
 
 	</main><!-- #main -->
 
 <?php
-get_footer();
+get_footer('logo');
