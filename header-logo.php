a1dc5f457f0f25be069b10bc765a39977c312fa6
OttavioM <OttavioM@users.noreply.github.com>
2023-04-27 13:49:55 +0200

    Update header-logo.php
    new header with logo

--- header-logo.php
+++ header-logo.php
@@ -15,7 +15,7 @@
 <head>
 	<!-- CODICI MIEI -->
 	<!-- <script src= <?php echo get_template_directory_uri() .'/js/hideHeaderOnScroll.js'; ?>> </script> -->
-	
+
 	<!-- NON TOCCARE -->
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -30,51 +30,19 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pasol' ); ?></a>
 	
 	<header id="masthead" class="site-header">
-
-		<div class="container pt-2 pb-2">
-			<div class="row align-items-center">
-				
-				<!-- <div class="col-md-5 ">
-					<?php aws_get_search_form( true ); ?>
-				</div> -->
-
-				<div class="col site-header__logo d-flex justify-content-center pt-3">
-					<!-- <?php the_custom_logo();  ?> -->
-					
-				</div>
-
-			</div>
+	<div class="row">
+		<div class="col-4 site-header__logo d-flex justify-content-center pt-3">
+			<?php the_custom_logo();  ?>		
 		</div>
 
-		<!-- <div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$pasol_description = get_bloginfo( 'description', 'display' );
-			if ( $pasol_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $pasol_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div> --> <!--.site-branding -->
-
-		<!-- MENU NAVIGATION -->
+		<div class="col-6">
 		<nav id="site-navigation" class="main-navigation">
-			<div class="container d-flex justify-content-center">
+			<div class="container d-flex justify-content-end">
 				<div class="row">
-					<div class="col-12 d-felx justify-content-center">
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
 							<i class="bi bi-list"></i>
 							<?php esc_html_e( 'Menu', 'pasol' ); ?>
 						</button>
-					</div>
  
 					<div class="col-12 text-center">
 						<?php
@@ -90,7 +58,8 @@
 				</div>
 			</div>
 		</nav><!-- #site-navigation -->
+		<div>
 
-
-		<?php echo '<hr class="gray-line">';?>
+			</div> <!-- row -->
+			<!-- <?php echo '<hr class="gray-line">';?> -->
 	</header><!-- #masthead -->
