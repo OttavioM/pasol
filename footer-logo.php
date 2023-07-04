0bf7b92b6668f014ad145aa725342b836253ac92
OttavioM <OttavioM@users.noreply.github.com>
2023-07-04 21:28:32 +0200

    Update footer-logo.php
    prova git hostinger

--- footer-logo.php
+++ footer-logo.php
@@ -35,7 +35,7 @@
 						About </br>
 						<?php
 							$about_links = create_links(
-								'About Us', 'https://www.example.com/contact',
+								'About Usys', 'https://www.example.com/contact',
 								'Our Partners', 'https://www.example.com/faqs',
 								'Reef Planting', 'https://www.example.com/docs',
 								'Tree Planting', 'https://www.example.com/docs'
