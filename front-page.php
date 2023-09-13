82c7e698fea5b09a91bfb6e106d6c7b15f5b0bd7
OttavioM <ottavio.mazzaretto@gmail.com>
2023-09-14 00:03:19 +0200

    collapse botton isntead

--- front-page.php
+++ front-page.php
@@ -146,84 +146,19 @@ get_header('logo');
 
 		<!-- <?php front_page_post("/img/reef_proj/reef_protection.jpg ","ciao","testo","white") ?> -->
 		
-		<div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-		<h2 class="accordion-header" id="headingOne">
-		  <button
-			class="accordion-button"
-			type="button"
-			data-mdb-toggle="collapse"
-			data-mdb-target="#collapseOne"
-			aria-expanded="true"
-			aria-controls="collapseOne"
-		  >
-			Accordion Item #1
-		  </button>
-		</h2>
-		<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-mdb-parent="#accordionExample">
-		  <div class="accordion-body">
-			<strong>This is the first item's accordion body.</strong> It is hidden by default,
-			until the collapse plugin adds the appropriate classes that we use to style each
-			element. These classes control the overall appearance, as well as the showing and
-			hiding via CSS transitions. You can modify any of this with custom CSS or
-			overriding our default variables. It's also worth noting that just about any HTML
-			can go within the <strong>.accordion-body</strong>, though the transition does
-			limit overflow.
-		  </div>
-		</div>
-	  </div>
-	  <div class="accordion-item">
-		<h2 class="accordion-header" id="headingTwo">
-		  <button
-			class="accordion-button collapsed"
-			type="button"
-			data-mdb-toggle="collapse"
-			data-mdb-target="#collapseTwo"
-			aria-expanded="false"
-			aria-controls="collapseTwo"
-		  >
-			Accordion Item #2
-		  </button>
-		</h2>
-		<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-mdb-parent="#accordionExample">
-		  <div class="accordion-body">
-			<strong>This is the second item's accordion body.</strong> It is hidden by
-			default, until the collapse plugin adds the appropriate classes that we use to
-			style each element. These classes control the overall appearance, as well as the
-			showing and hiding via CSS transitions. You can modify any of this with custom CSS
-			or overriding our default variables. It's also worth noting that just about any
-			HTML can go within the <strong>.accordion-body</strong>, though the transition
-			does limit overflow.
-		  </div>
-		</div>
-	  </div>
-	  <div class="accordion-item">
-		<h2 class="accordion-header" id="headingThree">
-		  <button
-			class="accordion-button collapsed"
-			type="button"
-			data-mdb-toggle="collapse"
-			data-mdb-target="#collapseThree"
-			aria-expanded="false"
-			aria-controls="collapseThree"
-		  >
-			Accordion Item #3
-		  </button>
-		</h2>
-		<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-mdb-parent="#accordionExample">
-		  <div class="accordion-body">
-			<strong>This is the third item's accordion body.</strong> It is hidden by default,
-			until the collapse plugin adds the appropriate classes that we use to style each
-			element. These classes control the overall appearance, as well as the showing and
-			hiding via CSS transitions. You can modify any of this with custom CSS or
-			overriding our default variables. It's also worth noting that just about any HTML
-			can go within the <strong>.accordion-body</strong>, though the transition does
-			limit overflow.
-		  </div>
-		</div>
-	  </div>
-	</div>
-	
+		<p>
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Enlace con href
+  </a>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Botón con data-bs-target
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Algún contenido placeholder para el componente de contracción. Este panel está oculto de forma predeterminada, pero se revela cuando el usuario activa el disparador correspondiente.
+  </div>
+</div>
 	</main><!-- #main -->
 
 <?php
