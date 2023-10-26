320be80f531e9c4096a1b3358061e843f7f64b9b
OttavioM <ottavio.mazzaretto@gmail.com>
2023-10-26 19:22:11 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -150,8 +150,8 @@ add_action( 'widgets_init', 'pasol_widgets_init' );
  * Enqueue scripts and styles.
  */
 function pasol_scripts() {
-    echo " this is the template URI" .get_template_directory_uri();
-    echo " this is the stylesheet URI" .get_stylesheet_uri();
+    echo " this is the template URI" . get_template_directory_uri();
+    echo " this is the stylesheet URI" . get_stylesheet_uri();
 
 	wp_enqueue_style( 'pasol-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_enqueue_style( 'pasol-main', get_template_directory_uri() . '/css/main.css');
