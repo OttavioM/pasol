abd774bf91f2f9b1f5f228a80696a880c35cb5cc
OttavioM <ottavio.mazzaretto@gmail.com>
2023-10-26 19:21:05 +0200

    functions.php- print stylesheet uri and template uri

--- functions.php
+++ functions.php
@@ -150,6 +150,9 @@ add_action( 'widgets_init', 'pasol_widgets_init' );
  * Enqueue scripts and styles.
  */
 function pasol_scripts() {
+    echo " this is the template URI" .get_template_directory_uri();
+    echo " this is the stylesheet URI" .get_stylesheet_uri();
+
 	wp_enqueue_style( 'pasol-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_enqueue_style( 'pasol-main', get_template_directory_uri() . '/css/main.css');
 	wp_enqueue_style( 'boostrap-icons', "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css");
